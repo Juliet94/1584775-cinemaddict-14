b1a49bb977507a3a9d3bb2ce2d76f51564bb3fd2
Anastasia <70826807+Juliet94@users.noreply.github.com>
2021-05-18 00:46:59 +0300

    Реализованы блоки Top Rated и Most Commented

--- src/utils/common.js
+++ src/utils/common.js
@@ -32,3 +32,7 @@ export const sortByRating = (cardA, cardB) => {
 export const sortByDate = (cardA, cardB) => {
   return dayjs(cardB.production).diff(dayjs(cardA.production));
 };
+
+export const sortByCommentsLength = (cardA, cardB) => {
+  return (getCommentLength(cardB.comments)) - (getCommentLength(cardA.comments));
+};
